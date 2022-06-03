d1c1263f2d6ae8ebff0363eb1842310bbbf625c1
diego <diego.diaz.armas@gmail.com>
2022-06-02 23:42:28 -0300

    "Entrar"

---
+++ init.js
@@ -0,0 +1,115 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    
+    scale: {
+        
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+      },
+  
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', '/primero/assets/sky.png');
+
+    
+        this.load.image('ground', '/primero/assets/platform.png');
+        this.load.image('star', '/primero/assets/star.png');
+        this.load.image('bomb', '/primero/assets/bomb.png');
+        this.load.spritesheet('dude', '/primero/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+var platforms;
+var player;
+var cursors;
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
+
+   // '
+
+  
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.collider(player, platforms);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+}
